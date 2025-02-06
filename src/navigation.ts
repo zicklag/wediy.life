f59681ce41c383838f3187f2786660acd61d6b2a
Zicklag <zicklag@katharostech.com>
2025-02-06 11:21:32 -0600

    feat: start drafting WeDIY content.

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,7 +1,24 @@
-import { getBlogPermalink } from './utils/permalinks';
+import { getPermalink } from './utils/permalinks.ts';
 
 export const headerData = {
   links: [
+    {
+      text: 'Home',
+      href: getPermalink('/'),
+    },
+    {
+      text: 'Projects',
+      links: [
+        {
+          text: 'All',
+          href: getPermalink('/projects'),
+        },
+        {
+          text: 'Foundation',
+          href: getPermalink('/projects/foundation'),
+        },
+      ],
+    },
     // {
     //   text: 'Homes',
     //   links: [
@@ -85,38 +102,39 @@ export const headerData = {
     //     },
     //   ],
     // },
-    {
-      text: 'Blog',
-      href: getBlogPermalink(),
-      // links: [
-      //   {
-      //     text: 'Blog List',
-      //     href: getBlogPermalink(),
-      //   },
-      //   {
-      //     text: 'Article',
-      //     href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-      //   },
-      //   {
-      //     text: 'Article (with MDX)',
-      //     href: getPermalink('markdown-elements-demo-post', 'post'),
-      //   },
-      //   {
-      //     text: 'Category Page',
-      //     href: getPermalink('tutorials', 'category'),
-      //   },
-      //   {
-      //     text: 'Tag Page',
-      //     href: getPermalink('astro', 'tag'),
-      //   },
-      // ],
-    },
+    // {
+    //   text: 'Blog',
+    //   href: getBlogPermalink(),
+    //   // links: [
+    //   //   {
+    //   //     text: 'Blog List',
+    //   //     href: getBlogPermalink(),
+    //   //   },
+    //   //   {
+    //   //     text: 'Article',
+    //   //     href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+    //   //   },
+    //   //   {
+    //   //     text: 'Article (with MDX)',
+    //   //     href: getPermalink('markdown-elements-demo-post', 'post'),
+    //   //   },
+    //   //   {
+    //   //     text: 'Category Page',
+    //   //     href: getPermalink('tutorials', 'category'),
+    //   //   },
+    //   //   {
+    //   //     text: 'Tag Page',
+    //   //     href: getPermalink('astro', 'tag'),
+    //   //   },
+    //   // ],
+    // },
     // {
     //   text: 'Widgets',
     //   href: '#',
     // },
   ],
-  // actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+  actions: [{ text: 'Support Us', href: getPermalink('/support') }],
+  showRssFeed: false,
 };
 
 export const footerData = {
@@ -175,7 +193,7 @@ export const footerData = {
     // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/katharostech' },
+    // { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/katharostech' },
   ],
   // footNote: `
   //   <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
