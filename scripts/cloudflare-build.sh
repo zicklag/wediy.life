83ace85b5320aecc016848b01419c17092a1193a
Zicklag <zicklag@katharostech.com>
2025-04-03 09:39:28 -0500

    ci: add environment variables to cloudflare build.

--- scripts/cloudflare-build.sh
+++ scripts/cloudflare-build.sh
@@ -6,6 +6,11 @@ set -ex
 curl -fsSL https://deno.land/install.sh | sh
 deno="$HOME/.deno/bin/deno"
 
+# Configure environment
+echo "PUBLIC_TURNSTILE_SITE_KEY=$TURNSTILE_SITE_KEY" >> .env.production
+echo "TURNSTILE_SECRET_KEY=$TURNSTILE_SECRET_KEY" >> .env.production
+echo "PUBLIC_CONTACT_WEBHOOK=$CONTACT_WEBHOOK" >> .env.production
+
 # Install dependencies
 $deno install --allow-scripts
 $deno run build
